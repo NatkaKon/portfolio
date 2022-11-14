e5fd898660ca5e328817cc7675417261151f9117
Natalia Kondratieva <kondratieva07@gmail.com>
2022-11-14 21:36:07 +0300

    create footer module

---
+++ src/footer/Footer.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import style from './Footer.module.css'
+import styleContainer from '../common/styles/Container.module.css';
+
+
+export const Footer = () => {
+    return (
+        <div className={style.footer}>
+            <div className={`${styleContainer.container} ${style.footerContainer}`}>
+                <h3>Natalia Kondratieva</h3>
+                <div className={style.footerLinks}>
+                    <a className={style.link} href="https://github.com/NatkaKon">
+                        <img className={style.icon} src="" alt="Icon Github"/></a>
+                    <a className={style.link} href="https://github.com/NatkaKon">
+                        <img className={style.icon} src="" alt="Icon Github"/></a>
+                    <a className={style.link} href="https://github.com/NatkaKon">
+                        <img className={style.icon} src="" alt="Icon Github"/></a>
+                    <a className={style.link} href="https://github.com/NatkaKon">
+                        <img className={style.icon} src="" alt="Icon Github"/></a>
+                </div>
+                <p> &#169; 2022</p>
+
+            </div>
+        </div>
+    );
+};
+
