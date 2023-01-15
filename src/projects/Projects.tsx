f138464598496ae6bfaa1b6629abec773a7fcad3
Natalia Kondratieva <kondratieva07@gmail.com>
2023-01-15 21:01:49 +0300

    update projects block

--- src/projects/Projects.tsx
+++ src/projects/Projects.tsx
@@ -3,17 +3,24 @@ import style from './Projects.module.css'
 import styleContainer from '../common/styles/Container.module.css'
 import {Project} from './project/Project';
 import {Title} from '../common/components/title/Title';
+import todoImg from '../assets/img/todolist.jpg'
+import socialNetwork from '../assets/img/socialNetwork.png'
 
 export const Projects = () => {
+
+    const todolistPreview={
+        backgroundImage:`url(${todoImg})`
+    }
+    const socialNetworkPreview={
+        backgroundImage:`url(${socialNetwork})`
+    }
     return (
         <div className={style.projectsBlock}>
             <div className={`${styleContainer.container} ${style.projectsContainer}`}>
                 <Title titleBlocks={'Projects'}/>
                 <div className={style.projects}>
-                    <Project title={'TODO'} description={'todo description'}/>
-                    <Project title={'Portfolio'} description={'portfolio description'}/>
-                    <Project title={'Tic-tac-toe'} description={'tic-tac-toe description'}/>
-                    <Project title={'Tic-tac-toe'} description={'tic-tac-toe description'}/>
+                    <Project style={todolistPreview} title={'TODO'} description={'todo description'}/>
+                    <Project style={socialNetworkPreview} title={'Social Network'} description={'socialNetwork description'}/>
                 </div>
             </div>
         </div>
--- src/projects/project/Project.tsx
+++ src/projects/project/Project.tsx
@@ -3,17 +3,21 @@ import style from './Project.module.css'
 
 type ProjectPropsType = {
     title: string
-    description:string
+    description: string
+    style:any
 }
 
 export const Project = (props: ProjectPropsType) => {
     return (
         <div className={style.project}>
-            <div className={style.imgContainer}>
-                <a className={style.button}>Watch</a>
+            <div className={style.imgContainer} style={props.style}>
+                <a className={style.buttonProject}>Watch</a>
             </div>
-            <h4>{props.title}</h4>
-            <span className={style.description}>{props.description}</span>
+            <div className={style.projectInfo}>
+                <h3 className={style.projectTitle}>{props.title}</h3>
+                <span className={style.description}>{props.description}</span>
+            </div>
+
         </div>
     );
 };
